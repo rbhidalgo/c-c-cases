ec809b72d3591522b84035f1e44134b992d6508f
Rogelio <rhidalgo987@gmail.com>
2023-01-25 23:42:49 -0800

    doc update

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   render() {
@@ -8,13 +9,15 @@ class MyDocument extends Document {
         <link 
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" 
           rel="stylesheet"/>
+          <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-H6QETN9482"/>
         {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
+        <Script strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            gtag('config', 'G-H6QETN9482');
           `,
           }}
         />

cd007b51f0374df1f14f42a64c4be86c7c4b4fa6
Rogelio <rhidalgo987@gmail.com>
2023-02-02 20:25:27 -0800

    site updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Layout from '../components/Layout'
 import Head from 'next/head'
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 import '../styles/globals.scss'
 
 const App = ({Component, pageProps}) => {
@@ -12,9 +13,16 @@ const App = ({Component, pageProps}) => {
                 <meta
                     name="description"
                     content={Component.description ?? 'C&C Road Cases'}/>
-                <link rel='icon' href='/favicon.ico' />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="manifest" href="/site.webmanifest"/>
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000"/>
+                <meta name="msapplication-TileColor" content="#2b5797"/>
+                <meta name="theme-color" content="#ffffff"/>
             </Head>
             <Layout>
+                <GoogleAnalytics measurementId="G-H6QETN9482" />
                 <Component {...pageProps}/>
             </Layout>
         </>
--- pages/_document.js
+++ pages/_document.js
@@ -9,18 +9,6 @@ class MyDocument extends Document {
         <link 
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&display=swap" 
           rel="stylesheet"/>
-          <Script strategy="beforeInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-H6QETN9482"/>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-H6QETN9482');
-          `,
-          }}
-        />
         </Head>
         <body>
           <Main />

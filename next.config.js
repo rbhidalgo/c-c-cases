f3d09f77d3b35a7768ca0fae67db1c3bf4f76f35
Rogelio <rhidalgo987@gmail.com>
2023-02-11 10:55:06 -0800

    updates

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
  trailingSlash: true,
+ redirects: async () => [
+  {
+    source: '/:path*',
+    has: [{ type: 'host', value: 'www.ccroadcases.com' }],
+    destination: 'https://ccroadcases.com/:path*',
+    permanent: true
+  }
+ ]
 }
